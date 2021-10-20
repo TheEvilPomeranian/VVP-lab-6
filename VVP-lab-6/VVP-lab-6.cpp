0d410a87a1477e187092d41feb92b90367dee018
TheEvilPomeranian <90762280+TheEvilPomeranian@users.noreply.github.com>
2021-10-20 12:56:51 +0300

    Update VVP-lab-6.cpp

--- VVP-lab-6/VVP-lab-6.cpp
+++ VVP-lab-6/VVP-lab-6.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 	setlocale(LC_ALL, "ru");
-	cout << "Задача 1." << endl;
+	cout << "Задача 1. Обмен значений переменных A и B." << endl;
 	double A, B;
 	cout << "Введите значение для А: "; cin >> A;
 	cout << "Введите значение для В: "; cin >> B;
@@ -13,7 +13,7 @@ int main(){
 	cout << "A = " << A << endl;
 	cout << "B = " << B << endl;
 
-	cout << "\nЗадача 2." << endl;
+	cout << "\nЗадача 2. Cодержимое A в B, B — в C, C — в A." << endl;
 	double a, b, c, dop;
 	cout << "Введите значение для А: "; cin >> a;
 	cout << "Введите значение для В: "; cin >> b;
@@ -27,7 +27,7 @@ int main(){
 	cout << "B = " << b << endl;
 	cout << "C = " << c << endl;
 
-	cout << "\nЗадача 3." << endl;
+	cout << "\nЗадача 3. Cодержимое A в C, C — в B, B — в A." << endl;
 	double x, y, z, zap;
 	cout << "Введите значение для A: "; cin >> x;
 	cout << "Введите значение для B: "; cin >> y;
@@ -41,28 +41,28 @@ int main(){
 	cout << "B = " << y << endl;
 	cout << "C = " << z << endl; 
 
-	cout << "\nЗадача 4." << endl;
+	cout << "\nЗадача 4. Найти решение уравнения y = 3*x^6 - 6*x^2 - 7" << endl;
 	double X, Y;
 	cout << "Задайте значение для x: "; cin >> X;
 	Y = 3 * pow(X, 6) - 6 * pow(X, 2) - 7;
 	cout << "y = " << Y << endl; 
 
-	cout << "\nЗадача 5." << endl;
+	cout << "\nЗадача 5. Решите уравнение y = 4*x^6 - 7*x^3 + 2" << endl;
 	double r, t;
 	cout << "Задайте значение для x: "; cin >> r;
 	r = r - 3;
 	t = 4 * pow(r, 6) - 7 * pow(r, 3) + 2;
 	cout << "y = " << t << endl;
 
-	cout << "\nЗадача 6." << endl;
-	double AA, q;
+	cout << "\nЗадача 6. A^8" << endl;
+	long int AA, q;
 	cout << "Введите значение для А: "; cin >> AA;
 	q = pow(AA, 5); 
 	AA =q * AA * AA * AA;
 	cout << "A^8 = " << AA << endl;
 
-	cout << "\nЗадача 7." << endl;
-	double aa, s, f;
+	cout << "\nЗадача 7. A^15" << endl;
+	long int aa, s, f;
 	cout << "Введите значение для A: "; cin >> aa;
 	s = pow(aa, 6);
 	f = pow(aa, 5);
